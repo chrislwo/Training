9c208b6f76d70d5d93baf457c6eefd368d0459bb
chrislwo <43380573+chrislwo@users.noreply.github.com>
2018-09-19 18:39:18 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Hello = () => <h2>Hello World from HSBC </h2>
+
+const Image1 = (props) => {
+  let { src1, title } = props;
+  src1 = src1 || 'https://www.w3schools.com/html/pic_trulli.jpg';
+  title = title || "training Demo1";
+  return (
+    <img src={src1} alt={title} title={title} width="500" height="333" />
+  )
+}
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React from Chris</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <img src="https://www.w3schools.com/html/pic_trulli.jpg" alt="Trulli" width="500" height="333" title="1234"></img>
+        <Hello />
+        <Image1 />
+        <br />
+        <Image1 src1="https://www.w3schools.com/html/img_girl.jpg" title="test 2"/>
+        <div><Image1 src1="https://www.w3schools.com/html/img_chania.jpg" title="test 3"/></div>
+      </div>
+
+    );
+  }
+}
+``
+export default App;
