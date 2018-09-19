6a4de3e9674353d4d588ac9af82b0cd10b93e644
chrislwo <43380573+chrislwo@users.noreply.github.com>
2018-09-19 19:15:26 +0800

    add timer

--- src/App.js
+++ src/App.js
@@ -12,6 +12,36 @@ const Image1 = (props) => {
     <img src={src1} alt={title} title={title} width="500" height="333" />
   )
 }
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Seconds: {this.state.seconds}
+      </div>
+    );
+  }
+}
+
 class App extends Component {
 
   render() {
@@ -21,6 +51,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React from Chris</h1>
         </header>
+        <div>
+          <Timer />
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
